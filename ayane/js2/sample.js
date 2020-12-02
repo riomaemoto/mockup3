90063824ee8ae4cc2ad408bfcc22c83c702d9a34
下里彩音 <harmys10@gmail.com>
2020-12-02 21:49:55 +0900

    追加

--- ayane/js2/sample.js
+++ ayane/js2/sample.js
@@ -1,3 +1,4 @@
+// メインスライド
 function slideSwitch() {
   var $active = $('#slideshow img.active');
 
@@ -18,3 +19,25 @@ function slideSwitch() {
 $(function() {
   setInterval( "slideSwitch()", 3500 );
 });
+// ハンバーガーメニュー
+$(function() {
+  // ハンバーガーメニューが押された時
+  $('#toggle').on('click',function(){
+      $(this).toggleClass('on');
+      $('.js-drower').toggleClass('on');
+      
+      let isActive = $(this).hasClass('on');
+      toggleDrower(isActive);
+  });
+});
+
+//ハンバーガーメニューでドロップダウン
+  function toggleDrower(isActive) {
+    if (isActive) {
+      // onになっていた時、メニューを表示
+      $('#drower-bg').fadeIn(600);
+    } else {
+      // onを外した時、メニューを非表示
+      $('#drower-bg').fadeOut(600);
+    }
+  }
